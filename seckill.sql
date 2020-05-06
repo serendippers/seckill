b36198dd7f0482338ac35c9d0b9eebd676d16848
zhangpengpeng <zhan_gpen_g@163.com>
2020-05-06 12:01:01 +0800

    设计数据表，API message

---
+++ seckill.sql
@@ -0,0 +1,116 @@
+-- ----------------------------
+-- database seckill
+-- ----------------------------
+USE seckill;
+
+DROP TABLE IF EXISTS `goods`;
+
+-- ----------------------------
+-- Table structure for goods
+-- ----------------------------
+CREATE TABLE `goods` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '商品ID',
+  `goods_name` varchar(16) DEFAULT NULL COMMENT '商品名称',
+  `goods_title` varchar(64) DEFAULT NULL COMMENT '商品标题',
+  `goods_img` varchar(64) DEFAULT NULL COMMENT '商品的图片',
+  `goods_detail` longtext COMMENT '商品的详情介绍',
+  `goods_price` decimal(10,2) DEFAULT '0.00' COMMENT '商品单价',
+  `goods_stock` int(11) DEFAULT '0' COMMENT '商品库存，-1表示没有限制',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5;
+
+-- ----------------------------
+-- Records of goods
+-- ----------------------------
+INSERT INTO `goods` VALUES ('1', 'iphoneX', 'Apple iPhone X (A1865) 64GB 银色 移动联通电信4G手机', '/img/iphonex.png', 'Apple iPhone X (A1865) 64GB 银色 移动联通电信4G手机', '8765.00', '10000');
+INSERT INTO `goods` VALUES ('2', '华为Meta9', '华为 Mate 9 4GB+32GB版 月光银 移动联通电信4G手机 双卡双待', '/img/meta10.png', '华为 Mate 9 4GB+32GB版 月光银 移动联通电信4G手机 双卡双待', '3212.00', '-1');
+INSERT INTO `goods` VALUES ('3', 'iphone8', 'Apple iPhone 8 (A1865) 64GB 银色 移动联通电信4G手机', '/img/iphone8.png', 'Apple iPhone 8 (A1865) 64GB 银色 移动联通电信4G手机', '5589.00', '10000');
+INSERT INTO `goods` VALUES ('4', '小米6', '小米6 4GB+32GB版 月光银 移动联通电信4G手机 双卡双待', '/img/mi6.png', '小米6 4GB+32GB版 月光银 移动联通电信4G手机 双卡双待', '3212.00', '10000');
+
+-- ----------------------------
+-- Table structure for seckill_goods
+-- ----------------------------
+DROP TABLE IF EXISTS `seckill_goods`;
+CREATE TABLE `seckill_goods` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '秒杀的商品表',
+  `goods_id` bigint(20) DEFAULT NULL COMMENT '商品Id',
+  `seckill_price` decimal(10,2) DEFAULT '0.00' COMMENT '秒杀价',
+  `stock_count` int(11) DEFAULT NULL COMMENT '库存数量',
+  `start_date` datetime DEFAULT NULL COMMENT '秒杀开始时间',
+  `end_date` datetime DEFAULT NULL COMMENT '秒杀结束时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5;
+
+-- ----------------------------
+-- Records of seckill_goods
+-- ----------------------------
+INSERT INTO `seckill_goods` VALUES ('1', '1', '0.01', '9', '2020-12-04 21:51:23', '2020-12-31 21:51:27');
+INSERT INTO `seckill_goods` VALUES ('2', '2', '0.01', '9', '2020-12-04 21:40:14', '2020-12-31 14:00:24');
+INSERT INTO `seckill_goods` VALUES ('3', '3', '0.01', '9', '2020-12-04 21:40:14', '2020-12-31 14:00:24');
+INSERT INTO `seckill_goods` VALUES ('4', '4', '0.01', '9', '2020-12-04 21:40:14', '2020-12-31 14:00:24');
+
+
+-- ----------------------------
+-- Table structure for seckill_user
+-- ----------------------------
+DROP TABLE IF EXISTS `seckill_user`;
+CREATE TABLE `seckill_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '用户ID，手机号码',
+  `nickname` varchar(255) NOT NULL,
+  `password` varchar(32) DEFAULT NULL COMMENT 'MD5(MD5(pass明文+固定salt) + salt)',
+  `salt` varchar(10) DEFAULT NULL,
+  `head` varchar(128) DEFAULT NULL COMMENT '头像，云存储的ID',
+  `register_date` datetime DEFAULT NULL COMMENT '注册时间',
+  `last_login_date` datetime DEFAULT NULL COMMENT '上蔟登录时间',
+  `login_count` int(11) DEFAULT '0' COMMENT '登录次数',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15211111111;
+
+-- ----------------------------
+-- Records of seckill_user
+-- ----------------------------
+INSERT INTO `seckill_user` VALUES ('18912341238', '18612766138', 'b7797cce01b4b131b433b6acf4add449', '1a2b3c4d', null, '2019-01-09 17:08:16', null, '0');
+INSERT INTO `seckill_user` VALUES ('18912341239', '18612766139', 'b7797cce01b4b131b433b6acf4add449', '1a2b3c4d', null, '2019-01-09 17:17:21', null, '0');
+INSERT INTO `seckill_user` VALUES ('18912341240', '18612766139', 'b7797cce01b4b131b433b6acf4add449', '1a2b3c4d', null, '2019-01-11 11:35:39', null, '0');
+INSERT INTO `seckill_user` VALUES ('18912341241', '18612766141', 'b7797cce01b4b131b433b6acf4add449', '1a2b3c4d', null, '2019-01-11 11:36:23', null, '0');
+INSERT INTO `seckill_user` VALUES ('18912341242', '18612766145', 'b7797cce01b4b131b433b6acf4add449', '1a2b3c4d', null, '2019-01-11 11:38:29', null, '0');
+INSERT INTO `seckill_user` VALUES ('18912341243', '18612766122', 'b7797cce01b4b131b433b6acf4add449', '1a2b3c4d', null, '2019-01-11 11:41:52', null, '0');
+INSERT INTO `seckill_user` VALUES ('18912341244', '18612766133', 'b7797cce01b4b131b433b6acf4add449', '1a2b3c4d', null, '2019-01-11 11:43:24', null, '0');
+INSERT INTO `seckill_user` VALUES ('18912341245', '18612766444', 'b7797cce01b4b131b433b6acf4add449', '1a2b3c4d', null, '2019-01-11 13:44:29', null, '0');
+
+
+-- ----------------------------
+-- Table structure for seckill_order
+-- ----------------------------
+DROP TABLE IF EXISTS `seckill_order`;
+CREATE TABLE `seckill_order` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) DEFAULT NULL COMMENT '用户ID',
+  `order_id` bigint(20) DEFAULT NULL COMMENT '订单ID',
+  `goods_id` bigint(20) DEFAULT NULL COMMENT '商品ID',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `u_uid_gid` (`user_id`,`goods_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=5;
+
+
+
+
+
+-- ----------------------------
+-- Table structure for order_info
+-- ----------------------------
+DROP TABLE IF EXISTS `order_info`;
+CREATE TABLE `order_info` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) DEFAULT NULL COMMENT '用户ID',
+  `goods_id` bigint(20) DEFAULT NULL COMMENT '商品ID',
+  `delivery_addr_id` bigint(20) DEFAULT NULL COMMENT '收获地址ID',
+  `goods_name` varchar(16) DEFAULT NULL COMMENT '冗余过来的商品名称',
+  `goods_count` int(11) DEFAULT '0' COMMENT '商品数量',
+  `goods_price` decimal(10,2) DEFAULT '0.00' COMMENT '商品单价',
+  `order_channel` tinyint(4) DEFAULT '0' COMMENT '1pc，2android，3ios',
+  `status` tinyint(4) DEFAULT '0' COMMENT '订单状态，0新建未支付，1已支付，2已发货，3已收货，4已退款，5已完成',
+  `create_date` datetime DEFAULT NULL COMMENT '订单的创建时间',
+  `pay_date` datetime DEFAULT NULL COMMENT '支付时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10;
